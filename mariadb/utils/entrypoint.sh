971bfc34df03fc861b47b7103b6b7bd760164481
ZaineMinhas <zaineminhas5@gmail.com>
2022-10-04 17:02:01 +0200

    la passe

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/03 16:41:46 by zminhas           #+#    #+#              #
-#    Updated: 2022/10/04 12:38:17 by zminhas          ###   ########.fr        #
+#    Updated: 2022/10/04 17:00:00 by zminhas          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,9 +17,9 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 
 	mysql -e "UPDATE mysql.user SET Password=PASSWORD($MARIADB_PASSOWRD) WHERE User=’root’;"
 	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-	mysql -e "DELETE FROM mysql.user WHERE User='';"
-	mysql -e "DROP DATABASE test;"
-	mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+	#mysql -e "DELETE FROM mysql.user WHERE User='';"
+	#mysql -e "DROP DATABASE test;"
+	#mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
 
 	echo "create $MARIADB_DATABASE"
 	mysql -e "create database $MARIADB_DATABASE"
