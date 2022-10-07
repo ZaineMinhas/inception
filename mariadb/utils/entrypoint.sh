b03c1d9a97c829586fe6432f542caf3765df4d28
ZaineMinhas <zaineminhas5@gmail.com>
2022-10-07 12:00:59 +0200

    la passe

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -6,29 +6,45 @@
 #    By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/03 16:41:46 by zminhas           #+#    #+#              #
-#    Updated: 2022/10/05 11:36:12 by zminhas          ###   ########.fr        #
+#    Updated: 2022/10/07 11:58:01 by zminhas          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
+# Give owner and group for the the DB which is normally automatically created in this folder 
+chown -R mysql:mysql /var/lib/mysql
+
+#Just checking if the DB has been correctly created in the right path
 if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
-	service mysql start
+	service mysql start --datadir=/var/lib/mysql
+	echo "--Service started"
 
-	mysql -e "UPDATE mysql.user SET Password=PASSWORD($MARIADB_PASSOWRD) WHERE User='root"
-	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-	mysql -e "DELETE FROM mysql.user WHERE User=''"
-	mysql -e "DROP DATABASE test"
-	mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+	# Create a folder for the daemon (mysql server’s socket file)
+	mkdir -p /var/run/mysqld
+	# Setting up .pid if it's not automatically set 
+	# .sock file has been automatically created at this point
+	touch /var/run/mysqld/mysqlf.pid
 
-	echo "create $MARIADB_DATABASE"
-	mysql -e "create database $MARIADB_DATABASE"
-	mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
-	mysql -e "grant all privileges on $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%'"
+	# Execute the .sql to setup the database
+	eval "echo \"$(cat /tmp/config.sql)\"" | mariadb -u root
 
-	mysql -e "flush privileges;"
+	# Set MySQL root password (if you don't set it no password at all)
+	mysqladmin -u root password $MARIADB_ROOT_PASSWORD;
+	echo "--Password set"
 
-	service mysql stop
+	service mysql stop --datadir=/var/lib/mysql
+	echo "--Service stopped"
+else
+	# Create a folder for the daemon (mysql server’s socket file)
+	mkdir -p /var/run/mysqld
+	#Setting up .pid and .sock since they're not automatically set
+	touch /var/run/mysqld/mysqlf.pid
+	mkfifo /var/run/mk
 fi
 
+#Give owner and group to that too
+chown -R mysql:mysql /var/run/mysqld
+#Start the database daemon
+echo "--start DB daemon"
 mysqld_safe --datadir=/var/lib/mysql
--- wordpress/utils/entrypoint.sh
+++ wordpress/utils/entrypoint.sh
@@ -6,10 +6,59 @@
 #    By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/06 15:24:18 by zminhas           #+#    #+#              #
-#    Updated: 2022/10/07 11:44:16 by zminhas          ###   ########.fr        #
+#    Updated: 2022/10/07 12:00:01 by zminhas          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
+# The www.conf file is needed for communication with the server
+grep -E "listen = 9000" "/etc/php/7.3/fpm/pool.d/www.conf" > /dev/null 2>&1
 
+# If not found it's useless to modify it 
+if [ $? -ne 0 ]; then
+ 	echo "--Modifying configuration file"
+	# Replacing the listen part
+	 sed -i "s|.*listen = /run/php/php7.3-fpm.sock.*|listen = 9000|g" "/etc/php/7.3/fpm/pool.d/www.conf" 
+fi
+
+# Instead of writing a wp-config.php file ourselves we can just generate one using the wordpress cli
+rm -rf /var/www/html/wordpress/wp-config.php
+wp config create --dbname=$MARIADB_DATABASE \
+		--dbuser=$MARIADB_USER \
+		--dbpass=$MARIADB_PASSWORD \
+		--dbhost=$MARIADB_HOST \
+		--path="/var/www/html/wordpress/" \
+		--allow-root \
+		--skip-check
+
+# Wordpress installing
+if ! wp core is-installed --allow-root; then
+	echo "--Installing Wordpress"
+	wp core install --url="$WORDPRESS_URL" \
+					--title="$WORDPRESS_TITLE" \
+					--admin_user="$WORDPRESS_ADMIN_USER" \
+					--admin_password="$WORDPRESS_ADMIN_PWD" \
+					--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+					--skip-email \
+					--allow-root
+
+fi
+
+# Simple update for wordpress 
+echo "--Updating wordpress"
+wp plugin update --all --allow-root
+
+# Create user (check how simon does it)
+echo "--Creating example user"
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor \
+													--user_pass=$WORDPRESS_USER_PWD \
+													--allow-root
+# Create article example 
+echo "--Creating example article"
+wp post generate --count=1 --post_title="example-post" --allow-root
+
+# We need this FastCGI Process Manager to run wordpress but also so that the container keeps running
+# --nodaemonize == keep foreground
+echo "--Starting "
+php-fpm7.3 --nodaemonize
