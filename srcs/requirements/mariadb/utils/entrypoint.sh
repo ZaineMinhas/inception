41609c627ec8a19ab5e00603b602640f4a757fc5
ZaineMinhas <zaineminhas5@gmail.com>
2022-10-12 11:49:55 +0200

    la passe

--- srcs/requirements/mariadb/utils/entrypoint.sh
+++ srcs/requirements/mariadb/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/03 16:41:46 by zminhas           #+#    #+#              #
-#    Updated: 2022/10/10 17:37:04 by zminhas          ###   ########.fr        #
+#    Updated: 2022/10/12 11:47:58 by zminhas          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	service mysql start --datadir=/var/lib/mysql
 
 	echo "create $MARIADB_DATABASE"
-	eval "echo \"$(cat config.sql)\"" | mariadb -u root #execute le config.sql
+	eval "echo \"$(cat config.sql)\"" | mariadb -u root
 
 	service mysql stop --datadir=/var/lib/mysql
 fi
